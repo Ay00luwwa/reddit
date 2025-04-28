777b7708250106f62b39c06c7326e70458f0b34e
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2025-04-28 21:55:58 +0100

    Define item fields in RedditItem and implement parsing logic in RedditJobSpider

--- reddit/items.py
+++ reddit/items.py
@@ -7,6 +7,6 @@ import scrapy
 
 
 class RedditItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    url = scrapy.Field()
+    score = scrapy.Field()
--- reddit/spiders/reddit_job.py
+++ reddit/spiders/reddit_job.py
@@ -4,8 +4,20 @@ import scrapy
 class RedditJobSpider(scrapy.Spider):
     name = "reddit_job"
     allowed_domains = ["reddit.com"]
-    start_urls = ["https://reddit.com"]
+
+    start_urls = ['https://www.reddit.com/r/funny/']
 
     def parse(self, response):
-        pass
-        
+        print (response.css("a.title::text").extract())
+        print (response.css("a.title::attr(href)").extract())
+        print (response.css("div.score.unvoted::attr(title)").extract())
+
+        for item in zip(titles, hrefs, scores):
+
+            new_item = RedditItem()
+            
+            new_item['title'] = item[0]
+            new_item['url'] = item[1]
+            new_item['score'] = item[2]
+            
+            yield new_item
